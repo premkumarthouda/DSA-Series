a46ef9695ead40878a1d402fdf9fe87e1970b01d
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-08-28 07:31:56 +0530

    Create Nfact.cpp

---
+++ Recursion/Nfact.cpp
@@ -0,0 +1,18 @@
+//N -factorial
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumn(int n){
+    if(n==1) {
+        return 1;
+        
+    }
+    return sumn(n-1)*n;
+}
+int main() {
+  // write your code here...
+  int n;
+  cin >> n;
+  cout <<sumn(n) << endl;
+  return 0;
+}
